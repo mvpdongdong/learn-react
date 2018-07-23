f1509b583a501479568810e2169f68a999cc2306
shendong <1239921402@qq.com>
2018-07-23 23:58:32 +0800

    event bind

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Clock from './components/Clock';
+import Toggle from './components/Toggle';
 
 class App extends Component {
   render() {
@@ -16,6 +17,7 @@ class App extends Component {
         </p>
         <Welcome name="shendong"></Welcome>
         <Clock/>
+        <Toggle/>
       </div>
     );
   }
