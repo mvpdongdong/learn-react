2144d72a479e010bf29f4264eee3113e2d8ef4ab
申冬 <dongshen211299@sohu-inc.com>
2018-08-02 17:38:59 +0800

    remove app.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import './App.css';
+import './App.scss';
 import RouterMap from './routes';
 import registerServiceWorker from './registerServiceWorker';
 
