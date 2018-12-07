5430d0ea4e7d856aadf198e874bb88669cb886ee
申冬 <dongshen211299@sohu-inc.com>
2018-12-07 17:29:25 +0800

    add yarn

--- src/utils/redux.js
+++ src/utils/redux.js
@@ -26,13 +26,35 @@ function createStore (reducer, fn) {
   };
 }
 
+function bindActionCreator (actionCreator, dispatch) {
+  return function () {
+    return dispatch(actionCreator.apply(this, arguments));
+  };
+}
+
 function bindActionCreators (actionCreators, dispatch) {
-  const obj = {};
+  if (typeof actionCreators === 'function') {
+    return bindActionCreator(actionCreators, dispatch);
+  }
+
+  if (typeof actionCreators !== 'object' || actionCreators === null) {
+    throw new Error(
+      `bindActionCreators expected an object or a function, instead received ${
+        actionCreators === null ? 'null' : typeof actionCreators
+      }. ` +
+        'Did you write "import ActionCreators from" instead of "import * as ActionCreators from"?'
+    );
+  }
+
+  const boundActionCreators = {};
   const keys = Object.keys(actionCreators);
   keys.forEach(key => {
-    obj[key] = (...args) => dispatch(actionCreators[key](...args));
+    const actionCreator = actionCreators[key];
+    if (typeof actionCreator === 'function') {
+      boundActionCreators[key] = bindActionCreator(actionCreator, dispatch);
+    }
   });
-  return obj;
+  return boundActionCreators;
 }
 
 const combineReducers = (reducers) => {
