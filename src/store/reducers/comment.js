7279423626c9d62967f73cdca646dee0b54b89e1
申冬 <dongshen211299@sohu-inc.com>
2018-12-13 17:41:35 +0800

    add immutable immer

--- src/store/reducers/comment.js
+++ src/store/reducers/comment.js
@@ -1,24 +1,25 @@
+import produce from 'immer';
+
 // action types
 const INIT_COMMENTS = 'INIT_COMMENTS';
 const ADD_COMMENT = 'ADD_COMMENT';
 const DELETE_COMMENT = 'DELETE_COMMENT';
 
 // reducer
-export default function (state = [], action) {
-  switch (action.type) {
-    case INIT_COMMENTS:
-    // 初始化评论
-      return action.comments;
-    case ADD_COMMENT:
-    // 新增评论
-      return [...state, action.comment];
-    case DELETE_COMMENT:
-    // 删除评论
-      return [
-        ...state.slice(0, action.commentIndex),
-        ...state.slice(action.commentIndex + 1)
-      ];
-    default:
-      return state;
-  }
-}
+export default (state = [], action) =>
+  produce(state, draft => {
+    switch (action.type) {
+      case INIT_COMMENTS:
+        // 初始化评论
+        return action.comments;
+      case ADD_COMMENT:
+        // 新增评论
+        draft.push(action.comment);
+        break;
+      case DELETE_COMMENT:
+        // 删除评论
+        draft.splice(action.commentIndex, 1);
+        break;
+      default:
+    }
+  });
